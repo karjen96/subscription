740a92fb3fb834bf63259dfd40ce945f00a58835
karjen96 <53561986+karjen96@users.noreply.github.com>
2021-11-26 10:10:11 +0800

    Add files via upload

--- testing/src/main/java/com/example/testing/SubcriptionController.java
+++ testing/src/main/java/com/example/testing/SubcriptionController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,34 +29,29 @@ public class SubcriptionController {
 	}
 	
 	//Mock data testing 
-	@PostMapping(path = { "subscription/daily"})
-	public String subscriptionDaily(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session,
-			@RequestBody final String input) throws Exception {
-		
+	@GetMapping(path = { "/subscription/daily"})
+	public String subscriptionDaily(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session) throws Exception {
 		
 		CommonDTO commonDTO=new CommonDTO(10.00,SubscriptionType.DAILY,"01/09/2021","01/11/2021");
 		return subscriptionService.performSubscription(commonDTO, session, request);
 	}
 	
-	@PostMapping(path = { "subscription/weekly"})
-	public String subscriptionWeekly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session,
-			@RequestBody final String input) throws Exception {
+	@GetMapping(path = { "/subscription/weekly"})
+	public String subscriptionWeekly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session) throws Exception {
 		CommonDTO commonDTO=new CommonDTO(10.00,SubscriptionType.WEEKLY,"TUESDAY","01/09/2021","01/11/2021");
 
 		return subscriptionService.performSubscription(commonDTO, session, request);
 	}
 	
-	@PostMapping(path = { "subscription/monthly"})
-	public String subscriptionMonthly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session,
-			@RequestBody final String input) throws Exception {
+	@GetMapping(path = { "/subscription/monthly"})
+	public String subscriptionMonthly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session) throws Exception {
 		CommonDTO commonDTO=new CommonDTO(10.00,SubscriptionType.MONTHLY,"23","01/09/2021","01/11/2021");
 
 		return subscriptionService.performSubscription(commonDTO, session, request);
 	}
 	
-	@PostMapping(path = { "subscription/monthly/exceed"})
-	public String subscriptionExceedMonthly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session,
-			@RequestBody final String input) throws Exception {
+	@GetMapping(path = { "subscription/monthly/exceed"})
+	public String subscriptionExceedMonthly(final HttpServletRequest request, final HttpServletResponse response, final HttpSession session) throws Exception {
 		CommonDTO commonDTO=new CommonDTO(10.00,SubscriptionType.MONTHLY,"23","01/08/2021","01/11/2021");
 
 		return subscriptionService.performSubscription(commonDTO, session, request);
